3fa08466f2b2e43a7ea22ebc8225d4f80c5b71a1
Wojciech Nawrot <wojtek.nawrot@gmail.com>
2024-07-12 12:30:12 +0200

    shadcn/ui added

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+}
