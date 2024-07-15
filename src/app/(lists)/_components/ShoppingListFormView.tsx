79db5520ba834496af10823729994b7dde4381ab
Wojciech Nawrot <wojtek.nawrot@gmail.com>
2024-07-15 10:23:32 +0200

    ShoppingListForm update

--- src/app/(lists)/_components/ShoppingListFormView.tsx
+++ src/app/(lists)/_components/ShoppingListFormView.tsx
@@ -1,32 +1,81 @@
 'use client';
 
 import { Header } from "@/components/composite/Header";
-import { ShoppingListForm } from "@/app/(lists)/_components/ShoppingListForm";
-import { useState, useRef } from "react";
+import { useRef, useTransition, MouseEvent } from "react";
 import { ViewContent } from "@/components/composite/ViewContent";
 import { ShoppingList } from "@prisma/client";
+import { useFormState } from "react-dom";
+import { createShoppingList, updateShoppingList, deleteShoppingList } from "@/app/(lists)/_actions/shoppingListActions";
+import { useRouter } from "next/navigation";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export interface ShoppingListFormViewProps {
   list?: ShoppingList | null;
 }
 
-export const ShoppingListFormView = ({list}: ShoppingListFormViewProps) => {
+export const ShoppingListFormView = ({ list }: ShoppingListFormViewProps) => {
 
-  const [pending, setPending] = useState(false);
+  const [error, action] = useFormState(
+    list == null ? createShoppingList : updateShoppingList.bind(null, list.id),
+    {}
+  );
+
+  // const [pending, setPending] = useState(false);
   const formRef = useRef<HTMLFormElement>(null);
+
+  const [isPending, startTransition] = useTransition();
+  const [isDeletePending, startDeleteTransition] = useTransition();
+  const router = useRouter();
 
   const handleSave = () => {
     formRef.current?.requestSubmit()
+  }
+
+  const handleAction = (formData: FormData) => {
+    console.log('handleAction')
+    startTransition(async () => {
+      action(formData);
+    });
+
+  }
+
+  const handleDelete = (e: MouseEvent<HTMLButtonElement>) => {
+    if (!list) return;
+    e.preventDefault();
+    startDeleteTransition(async () => {
+      await deleteShoppingList(list.id);
+      router.push('/')
+    });
   }
 
   return (<>
     <Header
       title='Lists'
       left={{ text: 'Cancel', href: '/' }}
-      right={{ text: !pending ? 'Save' : 'Save...', disabled: pending, onClick: handleSave }}
+      right={{ text: !isPending ? 'Save' : 'Save...', disabled: isPending || isDeletePending, onClick: handleSave }}
     />
     <ViewContent>
-      <ShoppingListForm onPending={setPending} formRef={formRef} list={list}/>
+      <form className='space-y-8' action={handleAction} ref={formRef}>
+        <div className='space-y-2'>
+          <Label htmlFor="productName">Name</Label>
+          <Input id="productName" type="text" name="name" autoFocus required defaultValue={list?.name || ""}/>
+          {error.name && <div className="text-destructive">{error.name}</div>}
+        </div>
+
+        <Button type="submit" className='hidden'>NotVisible</Button> {/* Default action on enter*/}
+
+        {list && <div className='space-y-2'>
+          <Label htmlFor="productDelete">Delete</Label>
+          <Button
+            className='block w-full bg-destructive text-destructive-foreground'
+            onClick={handleDelete}
+            disabled={isDeletePending}
+          >{!isDeletePending ? 'Delete' : 'Delete...'}</Button>
+        </div>}
+
+      </form>
     </ViewContent>
   </>);
 }
