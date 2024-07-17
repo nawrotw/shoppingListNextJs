134c6655614e8baeab266382d3d7194c5d4a5bc4
Wojciech Nawrot <wojtek.nawrot@gmail.com>
2024-07-17 17:05:48 +0200

    NavLink selection update

--- src/components/composite/navigation/NavLink.tsx
+++ src/components/composite/navigation/NavLink.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ReactNode, ComponentProps } from "react";
+import { ReactNode, ComponentProps, useMemo } from "react";
 import Link from "next/link";
 import { cn } from "@/lib/tailwindUtils";
 import { usePathname } from "next/navigation";
@@ -8,9 +8,15 @@ import { usePathname } from "next/navigation";
 type NavLinkProps = ComponentProps<typeof Link> & { icon: ReactNode, text: string };
 
 export function NavLink(props: NavLinkProps) {
-  const pathName = usePathname();
-  const isCurrentPath = pathName === props.href;
+
   const { className, icon, text, ...restProps } = props;
+
+  const pathName = usePathname();
+  const isCurrentPath = useMemo(
+    () => new RegExp(`^${props.href.toString()}`).test(pathName),
+    [pathName, props.href]
+  );
+
   return <Link {...restProps} className={cn(
     "flex-1 flex justify-center",
     "p-4 hover:bg-secondary hover:text-secondary-foreground",
