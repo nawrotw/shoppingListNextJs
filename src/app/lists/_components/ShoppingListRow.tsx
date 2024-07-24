01e296643d4c755d4243581645136f7c4a5167b0
Wojciech Nawrot <wojtek.nawrot@gmail.com>
2024-07-24 15:54:28 +0200

    Lists view update

--- src/app/lists/_components/ShoppingListRow.tsx
+++ src/app/lists/_components/ShoppingListRow.tsx
@@ -11,10 +11,12 @@ export interface ListItemProps {
 export const ShoppingListRow = (props: ListItemProps) => {
   const {item, actions, href} = props;
 
+  const toBuyCount = item.products.filter(({checked})=> !checked).length
+
   return <Link href={href} className='-mx-4 px-4 py-2 border-b flex items-center'>
     <div>
       <div className='text-xl'>{item.name}</div>
-      <div className='text-sm text-muted-foreground'>Products: {item.products.length}</div>
+      <div className='text-sm text-muted-foreground'>Products: {item.products.length}, to buy: {toBuyCount}</div>
     </div>
     <div className='flex flex-1 justify-end'>
       {actions}
